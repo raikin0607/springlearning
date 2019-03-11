f92c752f97e9d3b6e7bde685073348057d8b13af
HoangVM <hoangvm.work@gmail.com>
2019-03-11 17:59:46 +0700

    Update Exception Handle

--- src/main/java/learning/spring/boot/demo/exception/ProductExceptionController.java
+++ src/main/java/learning/spring/boot/demo/exception/ProductExceptionController.java
@@ -1,7 +1,15 @@
 package learning.spring.boot.demo.exception;
 
+import learning.spring.boot.demo.exception.product.ProductNotfoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ProductExceptionController {
+    @ExceptionHandler(value = ProductNotfoundException.class)
+    public ResponseEntity<Object> exception (ProductNotfoundException exception) {
+        return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+    }
 }
